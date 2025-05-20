9bea77663887e209d1fa6e8078b238736f48326e
Luis Leandro Rojas Rojas <rluisleandro@gmail.com>
2025-05-20 12:25:59 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://leandror2.github.io',
   devToolbar: {
     enabled: false
   },
